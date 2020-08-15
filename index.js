73e70f72cf028288fbfdde26b26210428ce4dbdc
zeepk <zeepkdb@gmail.com>
2020-08-15 14:39:30 -0700

    removed kontra

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
-let { init, Sprite, SpriteSheet, GameLoop, initKeys, keyPressed } = kontra;
-let { canvas, context } = init();
-
-const speed = 5;
-
-// this function must be called first before keyboard
-// functions will work
-initKeys();
-
-let image = new Image();
-image.src = 'assets/adventurer-run3-sword-Sheet.png';
-image.onload = function () {
-	// use spriteSheet to create animations from an image
-	let spriteSheet = SpriteSheet({
-		image: image,
-		frameWidth: 50,
-		frameHeight: 37,
-		animations: {
-			// create a named animation: walk
-			walk: {
-				frames: '0..5', // frames 0 through 9
-				frameRate: 15,
-			},
-		},
-	});
-
-	let sprite = Sprite({
-		x: 100,
-		y: 100,
-		anchor: { x: 0.5, y: 0.5 },
-
-		// required for an animation sprite
-		animations: spriteSheet.animations,
-	});
-
-	// use kontra.gameLoop to play the animation
-	let loop = GameLoop({
-		update: function (dt) {
-			if (keyPressed('left')) {
-				sprite.x -= speed;
-			}
-			if (keyPressed('right')) {
-				sprite.x += speed;
-			}
-			if (keyPressed('space')) {
-				sprite.y -= speed;
-			}
-
-			sprite.update();
-		},
-		render: function () {
-			sprite.render();
-		},
-	});
-
-	loop.start();
+const canvas = document.getElementById('canvas');
+const canvasRect = canvas.getBoundingClientRect();
+const width = canvas.clientWidth;
+const height = canvas.clientHeight;
+const speed = 10;
+var keyState = {};
+var x = width / 2;
+var y = height / 2;
+const randomInt = (min, max) => {
+	return Math.random() * (max - min) + min;
 };
+
+window.addEventListener(
+	'keydown',
+	function (e) {
+		keyState[e.keyCode || e.which] = true;
+	},
+	true
+);
+
+window.addEventListener(
+	'keyup',
+	function (e) {
+		keyState[e.keyCode || e.which] = false;
+	},
+	true
+);
+
+(function gameLoop() {
+	if ((keyState[37] || keyState[65]) && x > canvasRect.left) {
+		// left
+		x -= speed;
+	}
+	if (
+		(keyState[39] || keyState[68]) &&
+		x < canvasRect.right - sprite.clientWidth
+	) {
+		// right
+		x += speed;
+	}
+	if ((keyState[38] || keyState[87]) && y > canvasRect.top) {
+		// up
+		y -= speed;
+	}
+	if (
+		(keyState[40] || keyState[83]) &&
+		y < canvasRect.bottom - sprite.clientHeight
+	) {
+		// down
+		y += speed;
+	}
+
+	document.getElementById('sprite').style.left = x + 'px';
+	document.getElementById('sprite').style.top = y + 'px';
+	setTimeout(gameLoop, 10);
+})();
